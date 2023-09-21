d60dffc138e8cc808e24272b5360165d03bb1e92
Doniyor <doniyori@buffalo.edu>
2023-09-21 08:00:48 -0400

    init Frontend

--- src/components/landing-page.js
+++ src/components/landing-page.js
@@ -2,8 +2,8 @@ import React from 'react'
 function Landing() {
   return (
     <>
-    <div class="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 ...">
-      Hello
+    <div class="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 h-screen">
+      ORA
     </div>
     </>
   )

2d1c90cb499a6d7967adc4dc63999c77755d0990
Doniyor <doniyori@buffalo.edu>
2023-09-21 11:25:15 -0400

    Landing page skeleton

--- src/components/landing-page.js
+++ src/components/landing-page.js
@@ -1,12 +1,34 @@
-import React from 'react'
+import React from "react";
+import css from "../styles/landing-page.css";
+import arrow from "../assets/arrow.png";
 function Landing() {
   return (
     <>
-    <div class="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 h-screen">
-      ORA
-    </div>
+      <div class="bg h-screen px-10 md:px-20 pt-36 md:pt-50 lg:pt-60 pb-20">
+        <div className="text-white text-5xl">
+          <div className="mt-2">Hello,</div>
+          <div className="mt-2">
+            I'm <b>ORA</b>
+          </div>
+          <div className="mt-2">Your Organizational Responsive Assistant</div>
+        </div>
+        <div className="py-10">
+          <button class="transparent-button font-bold py-2 px-8 rounded-lg text-white text-bold">
+            Get Started
+          </button>
+        </div>
+        <div className="absolute inset-x-0 bottom-2 flex flex-col items-center justify-center text-white text-md text-center">
+          <p>About</p>
+          <div className="mt-auto">
+            <img src={arrow} className="h-8 w-9" alt="Arrow" />
+          </div>
+        </div>
+      </div>
+      <div>
+        
+      </div>
     </>
-  )
+  );
 }
 
-export default Landing
+export default Landing;

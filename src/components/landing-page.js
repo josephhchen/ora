8997111ed930b1221d197d0c0e822082ae426747
joechen43 <jchen526@buffalo.edu>
2023-09-22 07:25:56 -0400

    Fixed text

--- src/components/landing-page.js
+++ src/components/landing-page.js
@@ -1,41 +1,88 @@
-import React from "react";
-import css from "../styles/landing-page.css";
+import React, { useEffect, useState } from "react";
+import "../styles/landing-page.css";
 import arrow from "../assets/arrow.png";
-function Landing() {
-  const navigate = useNavigate();  // <-- Get navigate function
+import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-scroll';
 
-  // Function to navigate to Chat
+function Landing() {
+  const navigate = useNavigate();
+  const fullText = ["Hello!", "I'm", "ORA", "Your Organized Responsive Assistant"];
+  const [currentText, setCurrentText] = useState(["", "", "", ""]);
+  const [pulsate, setPulsate] = useState(false);
+
+  useEffect(() => {
+    let index = 0;
+    let subIndex = 0;
+
+    const intervalId = setInterval(() => {
+      if (index < fullText.length) {
+        setPulsate(true);  // Begin pulsating
+        const currentStr = fullText[index];
+        setCurrentText((prev) => {
+          const newArr = [...prev];
+          newArr[index] = currentStr.substring(0, subIndex);
+          return newArr;
+        });
+
+        subIndex++;
+        if (subIndex > currentStr.length) {
+          setPulsate(false); 
+          index++;
+          subIndex = 0;
+        }
+      } else {
+        clearInterval(intervalId);
+      }
+    }, 100);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
   const goToChat = () => {
     navigate('/chat');
-  }
+  };
 
 return (
-    <>
-      <div class="bg h-screen px-10 md:px-20 pt-36 md:pt-50 lg:pt-60 pb-20">
-        <div className="text-white text-5xl">
-          <div className="mt-2">Hello,</div>
-          <div className="mt-2">
-            I'm <b>ORA</b>
-          </div>
-          <div className="mt-2">Your Organizational Responsive Assistant</div>
+  <>
+    <div className="bg h-screen flex flex-col items-center justify-center relative">
+    {/* <div className="waveContainer">
+        <div className={pulsate ? "animated-orb pulsate" : "animated-orb"}></div>
+      </div> */}
+      <div className="inter-font text-white text-5xl text-center">
+        <div className="mt-2">{currentText[0]}</div>
+        <div className="flex justify-center mt-2">
+          <div>{currentText[1]}</div>
+          <div className="ml-2 non-inter-font">{currentText[2]}</div>
         </div>
-        <div className="py-10">
-          <button class="transparent-button font-bold py-2 px-8 rounded-lg text-white text-bold">
-            Get Started
-          </button>
-        </div>
-        <div className="absolute inset-x-0 bottom-2 flex flex-col items-center justify-center text-white text-md text-center">
-          <p>About</p>
-          <div className="mt-auto">
-            <img src={arrow} className="h-8 w-9" alt="Arrow" />
-          </div>
-        </div>
+        <div className="mt-2">{currentText[3]}</div>
       </div>
-      <div>
-        
+      <div className="py-10 flex justify-center">
+        <button 
+          className="transparent-button font-bold py-2 px-8 rounded-lg text-white text-bold"
+          onClick={goToChat}
+        >
+          Get Started
+        </button>
       </div>
-    </>
-  );
+      <Link
+          to="About"
+          spy={true}
+          smooth={true}
+          offset={0}
+          className="absolute inset-x-0 bottom-2 flex flex-col items-center justify-center text-white text-md text-center bounce">
+            <p>About</p>
+            <div className="mt-auto">
+              <img src={arrow} className="h-8 w-9" alt="Arrow" />
+            </div>
+        </Link>
+      </div>
+
+      <div className="h-screen" id="About">
+        About:
+
+      </div>
+  </>
+);
 }
 
 export default Landing;
